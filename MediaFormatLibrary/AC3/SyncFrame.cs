5edadd1146f42e8b13689d14626dd3ac3b459400
Tal Aloni <tal.aloni.il@gmail.com>
2017-08-19 13:28:59 +0300

    Minor documentation improvement

--- MediaFormatLibrary/AC3/SyncFrame.cs
+++ MediaFormatLibrary/AC3/SyncFrame.cs
@@ -15,7 +15,7 @@ namespace MediaFormatLibrary.AC3
     public class SyncFrame
     {
         // [ATSC Digital Audio Compression (AC-3) Standard]
-        // "Each synchronization frame contains 6 coded audio blocks (AB), each of which represent 256 new audio samples"
+        // "Each synchronization frame contains 6 coded audio blocks (AB), each of which represent 256 new audio samples per channel"
         public const uint SampleCount = 1536;
 
         public SyncInfo SyncInfo;
